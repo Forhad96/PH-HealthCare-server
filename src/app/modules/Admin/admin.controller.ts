3d60f7315077b984d7d757aeca546269db826281
Forhad96 <forhadairdrop@gmail.com>
2024-11-04 13:12:11 +0600

    update:get all admin api filter and search funtionality

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 import { AdminServices } from "./admin.service";
 
 const getAllAdminHandler = async (req: Request, res: Response) => {
-  const result = await AdminServices.getAllAdmin();
+// console.log(req.query);
+  const result = await AdminServices.getAllAdmin(req.query);
   res.status(200).json({
     success: true,
     message: "Admin data retrieves successfully",
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,8 +1,53 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const getAllAdmin = async () => {
-  const result = await prisma.admin.findMany();
+const getAllAdmin = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
+  console.log(filterData);
+
+  // [
+  //         {
+  //           name: {
+  //             contains: params.searchTerm,
+  //             mode: "insensitive",
+  //           },
+  //         },
+  //         {
+  //           email: {
+  //             contains: params.searchTerm,
+  //             mode: "insensitive",
+  //           },
+  //         },
+  //       ],
+  const andConditions: Prisma.AdminWhereInput[] = [];
+  const adminSearchAbleFields = ["name", "email", "contactNumber"];
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: adminSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
+  //   console.dir(andConditions,{depth:Infinity});
+
+  const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
+  });
+
   return result;
 };
 
